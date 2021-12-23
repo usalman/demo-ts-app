82b1beac4ec55f7017198ccbdf04111eb1a8caf8
usalman <umit.salman@bil.omu.edu.tr>
2021-12-23 15:05:57 +0300

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [notes, setNotes] = useState<{
+    id: string,
+    title: string,
+    text: string,
+    color: string,
+    date: string,
+  }[]>([{
+    id: (new Date).toString(),
+    title: 'meetings',
+    text: 'schedule meeting',
+    color: '#dfdfdf',
+    date: (new Date).toString()
+  }])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
---
+++ src/components/CreateNotes.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateNotes = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CreateNotes
