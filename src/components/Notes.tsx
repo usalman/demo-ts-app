c7cc92229906cea49833394266569d850ef05c77
usalman <umit.salman@bil.omu.edu.tr>
2021-12-23 16:21:57 +0300

    fill in the Notes comp. with data coming from the app comp.

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,12 +1,24 @@
 import * as React from "react";
+import { Button, Card } from "react-bootstrap";
 import { Note } from "../models/NoteModel";
 
 interface INotesProps {
   note: Note;
 }
 
-const Notes: React.FunctionComponent<INotesProps> = ({note}) => {
-  return <>{note.title}</>;
+const Notes: React.FC<INotesProps> = ({ note }) => {
+  return (
+    <div className="mb-3">
+      <Card style={{backgroundColor: note.color}}>
+        <Card.Body>
+          <Card.Title>{note.title}</Card.Title>
+          <Card.Text>{note.text}</Card.Text>
+          <Card.Subtitle className="text-muted">{note.date}</Card.Subtitle>
+          <Button className="mt-3" variant="danger">Delete</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 
 export default Notes;
